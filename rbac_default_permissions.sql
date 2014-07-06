b9f28fca6e40305e3c977bcca241fc74a8d9b6f5
Grumbo <blackwolfsden@hotmail.com>
2014-07-06 15:00:00 -0600

    Update rbac_default_permissions.sql

--- rbac_default_permissions.sql
+++ rbac_default_permissions.sql
@@ -1,35 +1,13 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.9-log - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             8.3.0.4694
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Dumping database structure for auth
-CREATE DATABASE IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `auth`;
-
-
--- Dumping structure for table auth.rbac_default_permissions
-CREATE TABLE IF NOT EXISTS `rbac_default_permissions` (
-  `secId` int(10) unsigned NOT NULL COMMENT 'Security Level id',
-  `permissionId` int(10) unsigned NOT NULL COMMENT 'permission id',
-  PRIMARY KEY (`secId`,`permissionId`),
-  KEY `fk__rbac_default_permissions__rbac_permissions` (`permissionId`),
-  CONSTRAINT `fk__rbac_default_permissions__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Default permission to assign to different account security levels';
-
--- Dumping data for table auth.rbac_default_permissions: ~1 rows (approximately)
 DELETE FROM `rbac_default_permissions`;
-/*!40000 ALTER TABLE `rbac_default_permissions` DISABLE KEYS */;
 INSERT INTO `rbac_default_permissions` (`secId`, `permissionId`) VALUES
-	(0, 55);
-/*!40000 ALTER TABLE `rbac_default_permissions` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+(0, 55), -- player
+(1, 71), -- gm1
+(2, 72), -- gm2
+(3, 73), -- gm3
+(4, 74), -- gm4
+(5, 196), -- admin
+(10, 60), -- VIP
+(11, 65), -- MVP
+(12, 70), -- gm helper
+(21, 80), -- developer
+(22, 194); -- moderator
