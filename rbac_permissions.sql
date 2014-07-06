97dc14094a86c70e8ad092573075982d7056a093
Grumbo <blackwolfsden@hotmail.com>
2014-07-06 14:45:30 -0600

    Update rbac_permissions.sql

--- rbac_permissions.sql
+++ rbac_permissions.sql
@@ -1,28 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.9-log - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             8.3.0.4694
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Dumping database structure for auth
-CREATE DATABASE IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `auth`;
-
-
--- Dumping structure for table auth.rbac_permissions
-CREATE TABLE IF NOT EXISTS `rbac_permissions` (
-  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Permission id',
-  `name` varchar(100) NOT NULL COMMENT 'Permission name',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Permission List';
-
--- Dumping data for table auth.rbac_permissions: ~871 rows (approximately)
 DELETE FROM `rbac_permissions`;
 /*!40000 ALTER TABLE `rbac_permissions` DISABLE KEYS */;
 INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
@@ -654,7 +629,3 @@ INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
 	(773, 'Command: wp reload'),
 	(774, 'Command: wp show'),
 	(850, 'World Chat');
-/*!40000 ALTER TABLE `rbac_permissions` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
